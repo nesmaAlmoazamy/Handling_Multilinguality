b71dc6ebe550919de67ff7ff6e8ed900721dc1be
Nesma Almoazamy <nesma.almoazamy93@gmail.com>
2019-06-10 10:48:07 +0300

    Create predictor.py

---
+++ AllenNlp/predictor.py
@@ -0,0 +1,21 @@
+from overrides import overrides
+
+from allennlp.common.util import JsonDict
+from allennlp.data import Instance
+from allennlp.predictors.predictor import Predictor
+
+
+@Predictor.register('myPredictor')
+class TextClassifierPredictor(Predictor):
+    """
+    Predictor for any model that takes in a sentence and returns
+    a single class for it.  In particular, it can be used with
+    the :class:`~allennlp.models.basic_classifier.BasicClassifier` model
+    """
+    def predict(self, sentence: str) -> JsonDict:
+        return self.predict_json({"sentence": sentence})
+
+    @overrides
+    def _json_to_instance(self, json_dict: JsonDict) -> Instance:
+        sentence = json_dict["sentence"]
+        return self._dataset_reader.text_to_instance(sentence)
